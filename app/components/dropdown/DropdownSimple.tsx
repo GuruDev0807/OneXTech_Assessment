6ecadef0b7269835a193d5efea49b2bb8d3208fe
thomas0114 <thomas.k98114@gmail.com>
2024-01-24 12:55:00 -0500

    completed header

--- app/components/dropdown/DropdownSimple.tsx
+++ app/components/dropdown/DropdownSimple.tsx
@@ -1,6 +1,5 @@
 "use client";
-
-import * as React from "react";
+import { useState } from "react";
 
 import {
   DropdownMenu,
@@ -11,34 +10,43 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MdArrowDropDown } from "react-icons/md";
+import { MdArrowDropDown, MdArrowDropUp } from "react-icons/md";
 
 export function DropdownSimple({ textHead, data }: any) {
-  const [position, setPosition] = React.useState("car#01");
+  const [position, setPosition] = useState("car#01");
+  const [clicked, setClickedDown] = useState(false);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <div className="flex items-center cursor-pointer">
-          <div className="text-[16px] font-semibold not-italic leading-5 mr-[8px]">
-            {textHead}
+    <div className="flex cursor-pointer select-none">
+      <DropdownMenu onOpenChange={() => setClickedDown(!clicked)}>
+        <DropdownMenuTrigger asChild>
+          <div className="flex items-center">
+            <div className="text-[16px] font-semibold not-italic leading-5 mr-[8px]">
+              {textHead}
+            </div>
+            <div className="flex text-[20px]">
+              {clicked === false ? <MdArrowDropDown /> : <MdArrowDropUp />}
+            </div>
           </div>
-          <div className="flex text-[20px]">
-            <MdArrowDropDown />
-          </div>
-        </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-[150px] mt-[5px]">
-        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-          {data?.map((each: string, index: number) => {
-            return (
-              <DropdownMenuRadioItem value={`car#${index + 1}`} key={index}>
-                {`#${index + 1}` + " " + each}
-              </DropdownMenuRadioItem>
-            );
-          })}
-        </DropdownMenuRadioGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-[150px] mt-[5px]">
+          <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+            {data?.map((each: string, index: number) => {
+              return (
+                <DropdownMenuRadioItem
+                  value={`car#${index + 1}`}
+                  key={index}
+                  onClick={() => {
+                    setClickedDown(true);
+                  }}
+                >
+                  {`#${index + 1}` + " " + each}
+                </DropdownMenuRadioItem>
+              );
+            })}
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 }
--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -18,7 +18,20 @@ const Header = () => {
         </div>
       </div>
       <div className="flex items-center">
-        
+        <div className="flex mr-[32px]">
+          <DropdownSimple textHead={"Find EV Cars"} data={dataEVCarsList} />
+        </div>
+        <div className="flex items-center select-none">
+          <div className="text-[16px] font-semibold not-italic leading-5 cursor-pointer transition duration-200 hover:drop-shadow-[0px_1px_1px_rgb(0,0,0)]">
+            Log In
+          </div>
+          <div className="text-[16px] font-semibold not-italic leading-5 m-[0px_5px]">
+            /
+          </div>
+          <div className="text-[16px] font-semibold not-italic leading-5 cursor-pointer transition duration-200 hover:drop-shadow-[0px_1px_1px_rgb(0,0,0)]">
+            Sign Up
+          </div>
+        </div>
       </div>
     </div>
   );
